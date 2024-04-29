be3b9954fe9c8a858753c6510775a16a52c54936
GregShiner <greg@justifinetwork.com>
2024-04-28 22:14:32 -0400

    Cleanup

--- bin/main.ml
+++ bin/main.ml
@@ -5,15 +5,6 @@ let parse_file filename =
   let sexp = Sexp.load_sexp filename in
   Parse.parse sexp
 
-(* let parse_string str = *)
-(*   let sexp = Sexp.of_string str in *)
-(*   Parse.parse sexp *)
-
-(* let eval sexp = *)
-(*   let sexp = Sexp.of_string sexp in *)
-(*   Format.printf "Output: %a\n%!" Interp.Value.pp *)
-(*     (Interp.interp (Parse.parse sexp) Interp.mt_env) *)
-
 let () =
   let filename = Sys.argv.(1) in
   let exp = parse_file filename in
@@ -22,4 +13,4 @@ let () =
   Format.printf "Output: %a\n%!" Interp.Value.pp
     (Interp.interp exp Interp.mt_env)
 
-let () = assert (Hashtbl.length Interp.store = 1)
+let () = assert (Hashtbl.length Interp.store <= 1)
