fc0bc5d8b919b5a07bdb6115bbdaf73ed7730264
Greg Shiner <greg@gregshiner.com>
2025-06-10 15:52:03 -0400

    Make demo.sh optionally use bat

--- demo.sh
+++ demo.sh
@@ -1,10 +1,18 @@
-for demo in `ls -v demos/`; do
+# Check if user has bat installed. If not, use cat instead.
+if command -v bat  >/dev/null 2>&1
+then
+    cat="bat -l lisp"
+else
+    cat=cat
+fi
+
+for demo in ./demos/*; do
     clear
     echo "Running demo $demo"
-    bat -l lisp demos/$demo
+    $cat $demo
     echo "Press enter to continue"
     read
-    dune exec ocaml_borrow_checker -- demos/$demo
+    dune exec ocaml_borrow_checker -- $demo
     echo "Press enter to continue"
     read
 done
